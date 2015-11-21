450df8f1b2e2952c432a1b13983509e9b759c92d
Arya Irani <arya.irani@gmail.com>
2015-11-21 16:56:55 -0500

    comments in Discounter

--- src/main/scala/bundlepricing/Discounter.scala
+++ src/main/scala/bundlepricing/Discounter.scala
@@ -18,14 +18,12 @@ case class Discounter(bundles: Set[Bundle]) {
     * @return
     */
   def total(cart: NonEmptyMap[Item, Quantity]): Dollars = {
-    // import comparison operators
-    val quantityOrdering: Ordering[Quantity] = implicitly
-    val dollarsOrdering: Ordering[Dollars] = implicitly
-    import dollarsOrdering.{mkOrderingOps => dollarsOrderingOps}
-    import quantityOrdering.{mkOrderingOps => qtyOrderingOps}
+    // import comparison operators without them shadowing each other
+    import Dollars.ordering.{mkOrderingOps => dollarsOrderingOps}
+    import Quantity.ordering.{mkOrderingOps => qtyOrderingOps}
 
     /** Does the cart contain the bundle's items in sufficient quantity? */
-    def canUtilizeBundle(cart: Map[Item, Quantity], bundle: Bundle): Boolean =
+    def canApplyBundle(cart: Map[Item, Quantity], bundle: Bundle): Boolean =
       bundle.items.forall {
         case (product, neededQuantity) =>
           cart.get(product).map(cartQuantity => cartQuantity >= neededQuantity).getOrElse(false)
@@ -33,7 +31,7 @@ case class Discounter(bundles: Set[Bundle]) {
 
     /** Remove bundle item quantities from a cart */
     def applyBundle(cartRemaining: Map[Item, Quantity], bundle: Bundle): Map[Item, Quantity] = {
-      assert(canUtilizeBundle(cartRemaining, bundle))
+      require(canApplyBundle(cartRemaining, bundle), "Don't call applyBundle when canApplyBundle is false.")
 
       bundle.items.foldLeft(cartRemaining) {
         case (cart, (product, quantity)) =>
@@ -44,42 +42,41 @@ case class Discounter(bundles: Set[Bundle]) {
       }
     }
 
-    @tailrec
-    def loop(open: List[PartialResult], bestPrice: Dollars): Dollars = {
+    /** try applying combinations of bundles until the lowest price combination is determined */
+    @tailrec def loop(open: List[PartialResult], bestPrice: Dollars): Dollars = {
       open match {
         case Nil =>
           // no more candidate results
           bestPrice
 
         case PartialResult(EmptyCart(), _, subtotal) :: tail =>
-          // no more items in cart
+          // no more items in cart.  update the bestPrice if ours is better
           loop(tail, bestPrice min subtotal)
 
         case PartialResult(cartRemaining, Nil, subtotal) :: tail =>
-          // no more bundles to apply
+          // no more bundles to apply.  add up the remaining items and update the bestPrice if ours better
           loop(tail, bestPrice min (subtotal + undiscountedTotal(cartRemaining)(iterable.iterableSubtypeFoldable)))
 
         case PartialResult(cartRemaining, bundle :: moreBundles, subtotal) :: tail =>
-          def useBundle = // apply this bundle and remove applicable items from the cart
-            PartialResult(applyBundle(cartRemaining.toMap, bundle), bundle :: moreBundles, subtotal + bundle.price)
-          def ignoreBundle = // don't apply this bundle
+          def useBundleScenario = // apply this bundle and remove applicable items from the cart
+            PartialResult(applyBundle(cartRemaining, bundle), bundle :: moreBundles, subtotal + bundle.price)
+
+          def ignoreBundleScenario = // don't apply this bundle
             PartialResult(cartRemaining, moreBundles, subtotal)
 
-          if (canUtilizeBundle(cartRemaining, bundle))
-            loop(useBundle :: ignoreBundle :: tail, bestPrice)
+          if (canApplyBundle(cartRemaining, bundle))
+            loop(useBundleScenario :: ignoreBundleScenario :: tail, bestPrice)
           else
-            loop(ignoreBundle :: tail, bestPrice)
+            loop(ignoreBundleScenario :: tail, bestPrice)
       }
     }
 
+    /** ignore bundles for things we aren't buying */
+    def relevantBundles: Set[Bundle] = bundles.filter(canApplyBundle(cart.toMap, _))
+
+    // start with a full cart and the undiscounted total and begin search
     loop(
-      open = List(
-        PartialResult(
-          cart.toMap,
-          bundles.toList.filter(canUtilizeBundle(cart.toMap, _)), // eliminate irrelevant bundles
-          Dollars(0)
-        )
-      ),
+      open = List(PartialResult(cart.toMap, relevantBundles.toList, Dollars(0))),
       bestPrice = undiscountedTotal(cart.toNEL)
     )
   }
